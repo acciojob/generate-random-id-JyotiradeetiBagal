b9f67e401b5bfb171e592aabfe6027a1c082afe4
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-05 13:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function makeid(l) {
   // write your code here
+	var res="";
+	var characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*(){}[]|<>?/.,=-";
+
+	for(let i=0;i<l;i++){
+		res+=characters.charAt(
+			Math.floor(
+				Math.random()*characters.length;
+			)
+		)
+	}
+	return res;
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+
